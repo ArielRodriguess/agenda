4ffb93b62b87e97939dcc4d5a859242f9e80780c
ArielRodriguess <arielrodrigues7801@hotmail.com>
2025-07-30 14:17:42 -0300

    feat: registering new contacts

--- routes.js
+++ routes.js
@@ -2,6 +2,8 @@ const express = require("express")
 const router = express.Router()
 const homeController = require("./src/controllers/home")
 const loginController = require("./src/controllers/login")
+const contatoController = require("./src/controllers/contato")
+const { loginRequired } = require("./src/middlewares/middlewares")
 
 // Rotas da Home
 router.get("/", homeController.index)
@@ -11,5 +13,10 @@ router.get("/login/index", loginController.index)
 router.post("/login/register", loginController.register)
 router.post("/login/login", loginController.login)
 router.get("/login/logout", loginController.logout)
+
+// Rotas de contato
+router.get("/contato/index", loginRequired, contatoController.index)
+router.post("/contato/register", contatoController.register)
+router.get("/contato/index/:id", contatoController.editar)
 
 module.exports = router
---
+++ src/controllers/contato.js
@@ -0,0 +1,41 @@
+const Contato = require("../models/contato")
+
+exports.index = (req, res) => {
+  res.render("contato", {
+    contato: {},
+  })
+}
+
+exports.register = async (req, res) => {
+  try {
+    const contato = new Contato(req.body)
+    await contato.register()
+
+    if (contato.errors.length > 0) {
+      req.flash("errors", contato.errors)
+      req.session.save(() => res.redirect("/contato/index"))
+      return
+    }
+
+    req.flash("success", "Contato criado com sucesso.")
+    req.session.save(() =>
+      res.redirect(`/contato/index/${contato.contato._id}`)
+    )
+    return
+  } catch (error) {
+    return res.render("404")
+  }
+}
+
+exports.editar = async (req, res) => {
+  try {
+    if (!req.params.id) return res.render("404")
+
+    const contato = await Contato.buscarPorId(req.params.id)
+    if (!contato) return res.render("404")
+
+    res.render("contato", { contato })
+  } catch (error) {
+    console.error(error)
+  }
+}
